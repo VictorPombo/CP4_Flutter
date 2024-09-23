a2ba49bf4a31073126f303be2ba86d4978de8528
VictorPombo <99228259+VictorPombo@users.noreply.github.com>
2024-09-23 00:30:31 -0300

    Key

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 const imageUrl = 'https://image.tmdb.org/t/p/w500';
 const kBackgoundColor = Color(0xff121012);
 const kbuttonColor = Color.fromARGB(255, 89, 54, 133);
-const apiKey = 'bc2eaf44d94625d51533a17bd9e4f1f9';
+const apiKey = '53d953e416d5b9a66fa6bd95612224d8';
 Color backgroundPrimary = const Color.fromARGB(255, 171, 169, 180);
